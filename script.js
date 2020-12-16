482bd80bff4659c828a6ce5b97b041d8b2f88c8f
mtzngn <mt.zngn@gmail.com>
2020-12-16 17:57:15 +0000

    script started with variables and a button connected

---
+++ script.js
@@ -0,0 +1,10 @@
+// play against computer, the one who reaches to 25 first will win!
+//create the variables to be used in the game to avoid long lines
+let scoreKeeper1 = 0;
+let scoreKeeper2= 0;
+const roll = document.getElementById("roll");
+const hold = document.getElementById("hold");
+const newGame = document.getElementById("newGame");
+
+roll.addEventListener("click",()=>{
+})
